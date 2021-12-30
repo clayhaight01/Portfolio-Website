2a3c34be82847a168a48d63413e3e29d0aacade2
chaight <chaight@uwaterloo.ca>
2021-12-30 01:43:12 -0500

    Fixed opacity trigger

--- js/fade.js
+++ js/fade.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     
         $('.overlay').each( function(i){
             
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_object = $('.pcb_side').position().top + $('.pcb_side').outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             if(bottom_of_window > bottom_of_object ){
