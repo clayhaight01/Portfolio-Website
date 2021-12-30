b8184e84de6e4182cdd9b43e981a2b479227a4ca
chaight <chaight@uwaterloo.ca>
2021-12-30 01:09:46 -0500

    Smaller pictures

--- js/fade.js
+++ js/fade.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
-            if( bottom_of_window > bottom_of_object ){
-                $(this).animate({'opacity':'1'},1000);   
+            if(bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},250,"swing");         
             }
         }); 
     }); 
